942403f14b221073208ef58f71d29779661f4bb3
lktslionel <lktslionel@gmail.com>
2017-12-13 20:43:21 +0100

    Remove demo dir
    Hi from the template commit-msg hook

--- zenika-lille/20171213-intro-to-graphQL-API/code/demo/graphqljs/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = require('./app.js');
-const server = express();
-
-server.use('/graphql', app);
-server.listen(4000);
-
-console.log('Running a GraphQL API server at localhost:4000/graphql');
